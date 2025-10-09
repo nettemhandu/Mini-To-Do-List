46bbbc36ae0998a75f1d111888baa69c8b56b504
nettemhandu <antonetteliebe@icloud.com>
2025-10-09 18:15:41 +0200

    started javalin and added one route

---
+++ src/src/main/java/za/co/todo/TodoApp.java
@@ -0,0 +1,12 @@
+package za.co.todo;
+import io.javalin.Javalin;
+
+import java.lang.reflect.InaccessibleObjectException;
+
+public class TodoApp {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(7070);
+        app.get("/", ctx -> ctx.result("Hello, To-do App!"));
+        app.get("/tasks", ctx -> ctx.result("List of tasks will be here"));
+    }
+}
