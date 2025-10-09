65f6095d66e0959f00cbe9e0ebd1decb5c9d4201
nettemhandu <antonetteliebe@icloud.com>
2025-10-09 18:50:51 +0200

    implementing the todocontroller methods and array

--- src/src/main/java/za/co/todo/TodoController.java
+++ src/src/main/java/za/co/todo/TodoController.java
@@ -1,4 +1,13 @@
 package za.co.todo;
 
+import java.util.ArrayList;
+import io.javalin.http.Context;
+
 public class TodoController {
+    ArrayList<Task> tasks = new ArrayList<>();
+
+    public void showTasks(ctx){
+        ctx.json(tasks);
+    }
+
 }
