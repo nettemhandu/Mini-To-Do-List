7d0c37aa7d8bca817289a5f6fd6a067fc804f1a6
nettemhandu <antonetteliebe@icloud.com>
2025-10-12 13:59:07 +0200

    added tasks manually with controller constructor

--- src/src/main/java/za/co/todo/TodoApp.java
+++ src/src/main/java/za/co/todo/TodoApp.java
@@ -3,8 +3,11 @@ import io.javalin.Javalin;
 
 public class TodoApp {
     public static void main(String[] args) {
+
         Javalin app = Javalin.create().start(7070);
-        app.get("/", ctx -> ctx.result("Hello, To-do App!"));
-        app.get("/tasks", ctx -> ctx.result("List of tasks will be here"));
+
+        TodoController controller = new TodoController();
+        app.get("/", controller::showTasks);
+        app.get("/tasks", controller::showTasks);
     }
 }
--- src/src/main/java/za/co/todo/TodoController.java
+++ src/src/main/java/za/co/todo/TodoController.java
@@ -6,6 +6,12 @@ import io.javalin.http.Context;
 public class TodoController {
     ArrayList<Task> tasks = new ArrayList<>();
 
+    public TodoController(){
+        tasks.add(new Task(1, "Task 1", false));
+        tasks.add(new Task(2, "Task 2", false));
+        tasks.add(new Task(3, "Task 3", false));
+    }
+
     public void showTasks(Context ctx){
         ctx.json(tasks);
     }
