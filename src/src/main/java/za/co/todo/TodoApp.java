405b7a0f1f651105e57402b6b7360cb753f1c00f
nettemhandu <antonetteliebe@icloud.com>
2025-10-12 14:33:27 +0200

    initialized thymeleaf template

--- src/src/main/java/za/co/todo/TodoApp.java
+++ src/src/main/java/za/co/todo/TodoApp.java
@@ -1,13 +1,34 @@
 package za.co.todo;
 import io.javalin.Javalin;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
 
 public class TodoApp {
     public static void main(String[] args) {
+        // initialize Thymeleaf TemplateEngine
+        TemplateEngine templateEngine = new TemplateEngine();
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+        resolver.setPrefix("/templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML");
+        resolver.setCharacterEncoding("UTF-8");
+        templateEngine.setTemplateResolver(resolver);
 
+        // controller
+        TodoController controller = new TodoController();
+
+        // initialize Javalin
         Javalin app = Javalin.create().start(7070);
 
-        TodoController controller = new TodoController();
+        // set up routes
         app.get("/", controller::showTasks);
-        app.get("/tasks", controller::showTasks);
+        app.get("/tasks", ctx -> {
+            Context thymeleafContext = new Context();
+            thymeleafContext.setVariable("tasks", controller.tasks);
+            String html = templateEngine.process("tasks", thymeleafContext);
+            ctx.html(html);
+        });
     }
 }
